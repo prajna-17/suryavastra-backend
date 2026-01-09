a23ff45a85a12bbb5912e5591c46e853fa99164b
apoorva nayak <nayakapoorva2010@gmail.com>
2026-01-09 20:02:32 +0530

    email

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendOtpEmail = async (email, otp) => {
   await resend.emails.send({
-    from: "Suryavastra <onboarding@resend.dev>",
+    from: "Suryavastra <no-reply@suryavastra.com>",
     to: email,
     subject: "Your Suryavastra OTP",
     html: `<h2>Your OTP is ${otp}</h2><p>Valid for 5 minutes.</p>`,
